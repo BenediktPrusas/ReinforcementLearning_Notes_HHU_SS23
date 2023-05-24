5933dce75626e373b63a0101b10ba0dc8c4dc646
BenediktPrusas <benedikt.prusas@hs-duesseldorf.de>
2023-05-24 13:10:41 +0200

    Added links to Streamlit and Github

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reinforcement Learning Lecture SS23
+
+Welcome to the lecture notes of the Reinforcement Learning Lecture at HHU in SS23, taught by Prof. Andre Stuhlsatz.
+
+[View the lecture Notes on streamlit](https://benediktprusas-reinforcementlearning-notes-streamlit-app-4sic12.streamlit.app/)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+st.markdown("""
+# Reinforcement Learning Lecture SS23
+
+Welcome to the lecture notes of the Reinforcement Learning Lecture at HHU in SS23, taught by Prof. Andre Stuhlsatz.
+
+I hope this will become a joined effort of all students to create a comprehensive set of notes for the lecture.
+[Contribute on Github](https://github.com/BenediktPrusas/ReinforcementLearning_Notes_HHU_SS23)
+
+""")
